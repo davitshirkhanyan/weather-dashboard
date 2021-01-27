1a8c37c70aebf89b70f731de14ec10746337208f
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-01-27 10:27:05 -0800

    add function to get current uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-let cityName = "";
-
 // add function to search for cities
 $("#searchBtn").on("click", function() {
     let cityValue = $("input").val();
@@ -18,19 +16,47 @@ let cityWeatherInfo = function(cityValue) {
     request.onload = function() {
         let cityObj = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
-            cityName = cityObj.name;
+            let cityName = cityObj.name;
+            let lantitude = cityObj.coord.lat;
+            let longitude = cityObj.coord.lon;
+            
+            $("#cityName").text(`${cityName} (${moment().format("L")})`);
+            $("#cityName").append($("<img>").attr("src", "https://openweathermap.org/img/wn/" + cityObj.weather[0].icon + "@2x.png"));
+            $("#temp").attr("class", "font-weight-normal").html(` ${cityObj.main.temp}&#176F`);
+            $("#humidity").attr("class", "font-weight-normal").text(` ${cityObj.main.humidity}%`);
+            $("#windSpeed").attr("class", "font-weight-normal").text(` ${cityObj.wind.speed}MPH`);
+            uvIndexInfo(lantitude, longitude);
  } else if (cityValue === "") {
      alert("Please enter the name of the city");
  } else {
     alert(`Error: The city ${cityValue} doesn't exist!`);
  }
 }
-
-  
-       
-
-
    request.send();
-
 };
 
+// add function to get UV index
+let uvIndexInfo = function(lantitude, longitude) {
+    let apiKey = "46c83042b2cdf276d685079cb38dbf65";
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/uvi?lat=${lantitude}&lon=${longitude}&appid=${apiKey}`,
+        method: "GET"
+    }).then((response) =>{
+        let uvIndex = response.value;
+    
+        if(uvIndex <= 2) {
+            $("#uvIndex").append("<span>").attr("class", "bg-success rounded p-1 text-light").text(`Low - ${uvIndex}`);
+        }
+        else if(uvIndex > 2 && uvIndex <= 5) {
+            $("#uvIndex").append("<span>").attr("class", "bg-warning rounded p-1 text-light").text(`Moderate - ${uvIndex}`);
+        }
+        else if(uvIndex > 5 && uvIndex <= 7) {
+            $("#uvIndex").append("<span>").attr("class", "bg-danger rounded p-1 text-light").text(`High - ${uvIndex}`);
+        }
+        else if(uvIndex > 7 && uvIndex <= 10) {
+            $("#uvIndex").append("<span>").attr("class", "very-high rounded p-1 text-light").text(`Very High - ${uvIndex}`);
+        } else {
+            $("#uvIndex").append("<span>").attr("class", "bg-info rounded p-1 text-light").text(`Extreme - ${uvIndex}`);
+        }
+    });
+};
